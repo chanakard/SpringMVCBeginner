b05cee9937123508f4163331cb3124cd623040d9
chanakard <chanakaroshan86@gmail.com>
2020-07-02 05:50:41 +0530

    java based configuration spring mvc

---
+++ SpringmvcJava/src/main/java/com/personnel/springmvcjava/MyFormController.java
@@ -0,0 +1,25 @@
+package com.personnel.springmvcjava;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyFormController extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {MyConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
